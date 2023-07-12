fc857f5b309a77c89c01607be85daf36af8af067
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-07-12 21:55:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// once you press MArk as read,  if New messages ==true , remove toggle function and change color to white
+
+
+// messages with id new should have a red dot wheere the text is
+
+// Notification should increase according to the id of numbers with NEwmessages
+
+//when you click on message it takes you to your profilr
