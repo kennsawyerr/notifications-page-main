4205769039199a8878041c02aad6b764c39387ee
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-07-13 16:56:36 +0100

    modified:   index.html modified:   index.modified:   style.css

--- index.js
+++ index.js
@@ -6,3 +6,42 @@
 // Notification should increase according to the id of numbers with NEwmessages
 
 //when you click on message it takes you to your profilr
+
+let count = 0;
+
+// 
+// 
+// 
+const markAll = document.getElementById("mark-allmessages");
+ const White= "hsl(0, 0%, 100%)";
+
+const AlRead =document.getElementById("already-read");
+const list =document.querySelectorAll("li");
+
+const NotificationNum = document.getElementById("notifications-num")
+
+const newMsg = document.querySelectorAll(".new")
+
+
+
+   function clearCount(){
+if(newMsg){
+    NotificationNum.textContent= count
+}
+   }
+count= newMsg.length
+
+
+console.log( "Count:",count)
+
+
+
+
+
+markAll.addEventListener("click", function (){
+  list.forEach((e)=>{
+    e.style.backgroundColor=White
+  });
+  count=0;
+  clearCount()
+})
